2029beeea8105a6b61d1e8c9d25922bf617b5fbd
zhujinguang <zhujinguang@hisense.com>
2019-03-14 19:43:28 +0800

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  hiPodLib1Tests.m
+//  hiPodLib1Tests
+//
+//  Created by CrazyBeats on 03/14/2019.
+//  Copyright (c) 2019 CrazyBeats. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
